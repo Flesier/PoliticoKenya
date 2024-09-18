e47860ec4f69dcf0b3ffa28dd323b7df5c9be6e0
Flesier Akinyi <flesieronyango18@gmail.com>
2024-09-18 18:06:42 +0000

    make web scripts

--- index.js
+++ index.js
@@ -5,5 +5,16 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import { Platform } from 'react-native';
 
-AppRegistry.registerComponent(appName, () => App);
+if (Platform.OS === 'web') {
+    const rootElement = document.getElementById('root') || document.createElement('div');
+    rootElement.id = 'root';
+    document.body.appendChild(rootElement);
+    AppRegistry.runApplication(appName, {
+      initialProps: {},
+      rootTag: rootElement,
+    });
+  } else {
+    AppRegistry.registerComponent(appName, () => App);
+  }
